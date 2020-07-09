de035e67e5927eb2ecec0f78dd3276a991a5ffaa
Ed <edwoulfe@gmail.com>
2020-07-08 20:42:28 -0400

    Index page for consoling, and the first fundamental. Wanted to go over my fundamentals yet again during the pandemic to stay on top of my skills.

---
+++ anonymous-functions.js
@@ -0,0 +1,7 @@
+// Anonymous functions - assigning a variable name. We store the reference to the function, but the function itself does not have a name.
+
+let add = function (x, y) {
+    return x + y;
+}
+
+console.log(add(5, 6))
