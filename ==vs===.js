4e4ce2c8b6656419ee3a0e5ea38f155f030cd620
Ed <edwoulfe@gmail.com>
2020-07-24 12:56:34 -0400

    Double equals vs Triple equals. Checking values and types.

---
+++ ==vs===.js
@@ -0,0 +1,9 @@
+// Double equals checks the variables value.
+// Triple equals checks the variables value and type.
+
+let str = "21";
+let num = 21;
+
+console.log(str == num); // true
+console.log(str === num); // false
+console.log(21 === num); // true
