3bdc519819da8b1aa6a42d8849aeb8e2f0043e0b
Ed <edwoulfe@gmail.com>
2020-07-27 08:57:53 -0400

    typeof, checking the type of data. number, boolean, string, undefined, object, etc.. Some gotchas with arrays being objects.

---
+++ typeof.js
@@ -0,0 +1,8 @@
+console.log(typeof true); // boolean
+console.log(typeof 15); // number
+console.log(typeof "15"); // string
+console.log(typeof NaN); // number
+console.log(typeof {}); // object
+console.log(typeof [1, 2, 3, 4]); // object
+console.log(Array.isArray([1, 2, 3, 4, 5])); // true
+console.log(typeof undefined); // undefined
