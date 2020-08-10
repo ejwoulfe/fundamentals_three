0f375b465183db21eddca99a6a3ddf2563fbe6d2
Ed <edwoulfe@gmail.com>
2020-08-10 08:32:02 -0500

    Undefined, null, and undeclared.

---
+++ undefined-vs-null.js
@@ -0,0 +1,16 @@
+// Null is user defined where as undefined is trying to reach a variable that doesn't have a value.
+
+let x;
+
+console.log(x);
+
+
+
+// A good example of null would be in a database of a persons middle name. Some people want to enter that data, some don't.
+// So if they do not want to enter that data, it will just remain null. When reading from the database, we can just simply ignore it.
+let y = null;
+
+console.log(y);
+
+// Undeclared - doesn't exist.
+console.log(variable);
