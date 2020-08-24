c54b3cb5b2509b5be3512564938e83060026de40
Ed <edwoulfe@gmail.com>
2020-08-24 09:21:47 -0500

    Comparing Objects. Can be very complex or very simple depending on method.

---
+++ undeclared-vs-undefined.js
@@ -0,0 +1,12 @@
+// Undeclared - Variable doesn't exist. Was never declared inside of the code.
+
+// Undefined - Variable exists but has no value, not even null.
+
+let user;
+let person = {
+
+};
+
+console.log(user); // undefined
+console.log(person.firstName); // undefined
+console.log(car); // Uncaught ReferenceError: car is not defined
