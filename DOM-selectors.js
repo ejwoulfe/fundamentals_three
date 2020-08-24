09472928ba1c6622869c981e009c9dffe2f84e13
Ed <edwoulfe@gmail.com>
2020-08-24 08:55:14 -0500

    DOM selectors, selecting elements that are on the DOM. getElementById is probably the most used. Careful when using querySelectors, as they are slower.

---
+++ DOM-selectors.js
@@ -0,0 +1,7 @@
+document.getElementById('home-navigation');
+document.getElementsByClassName('random-class');
+document.getElementsByTagName('h1');
+
+// ***queryselector is slower***
+document.querySelector('h1.random-class');
+document.querySelectorAll('h1.random-class')[2];
