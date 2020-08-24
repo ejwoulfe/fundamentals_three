c6fb7ef94e554d6f13d21120bc5c800db13353f8
Ed <edwoulfe@gmail.com>
2020-08-24 08:40:09 -0500

    More details about arrow functions and the cautions we need to be aware of.

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,7 +1,7 @@
 // Arrow functions. Sugar syntanx for making functions. Better readability to some.
 // Does not bind this.
 
-add = (x, y) => {
+const add = (x, y) => {
     return x + y;
 }
 
@@ -15,3 +15,20 @@ let arr1 = [1, 2, 3, 4, 5];
 console.log(arr1.map(value => {
     return value + 1;
 }))
+
+
+const example = param1 => param1 + 2;
+
+console.log(example(2)); // 3
+
+
+const user = {
+    firstName: "Ed",
+    lastName: "Woulfe",
+    fullName: () => {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+
+console.log(user.fullName()) // undefined undefined
