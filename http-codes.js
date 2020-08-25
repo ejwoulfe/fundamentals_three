6b5f97aa19ba248c4580572966a773b45b1a02af
Ed <edwoulfe@gmail.com>
2020-08-25 09:21:43 -0500

    HTTP status codes. Important when dealing with communication. Client to serverside and vice versa.

---
+++ http-codes.js
@@ -0,0 +1,11 @@
+// 100, 200, 300, 400, 500
+
+// 100 - Information. Something is working.
+
+// 200 - Successful, we love to see these :)
+
+// 300 - Redirect
+
+// 400 - Error on user/client side. Reach something that doesn't exist.
+
+// 500 - Error on the server side. 
