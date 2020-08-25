8da14eee3f44e69bd1bdd0b752d17b647fb07130
Ed <edwoulfe@gmail.com>
2020-08-25 09:27:19 -0500

    Dealing with RESTFUL APIs is an important task when dealing with the front end. Make sure to know the routes and architecture of the api you are connecting with.

---
+++ REST.js
@@ -0,0 +1,8 @@
+// REST is what determines how the API looks. It is the rules that developers follow when they create an API.
+
+// Architectural patterns assisting in building APIs.
+
+// 1 - Versioning. Updating the front end with the current version of the backend.
+
+// Nouns - with http methods, like get users will return a list of users. where as a get users/1 will return only a single user. 
+// Know your routes!!
